a8e876876f1f2525229876fd313825bf3ccbfc66
ganesh <ganesh.hurgule@gmail.com>
2023-06-24 10:28:35 +0530

    employee create update operations created with view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,20 +1,25 @@
 class EmployeesController < ApplicationController
 
+    
+    def new
+        @employee =Employee.new()
+        render 'new'
+    end
     def create
-        employee = Employee.new(employee_params)
-        if employee.save
+        @employee = Employee.new(employee_params)
+        if @employee.save
             redirect_to employees_path
         else
             render 'new'
         end
     end
     def index
-        employees = Employee.all
-        render 'employees'
+        @employees = Employee.all
+        render 'index'
     end
     def edit 
         
-        employee = Employee.find(params[:id])
+        @employee = Employee.find(params[:id])
         
         render 'edit'
        
@@ -27,11 +32,12 @@ class EmployeesController < ApplicationController
             render 'edit'
         end
     end
-
+    
     private 
 
     def employee_params
-        params.require(:employee).permit(:firstname,:lastname,:email,:designation, :department_id)
-    end
+        params.require(:employee).permit(:first_name, :last_name, :email, :designation, :department_id)
+      end
+      
 
 end
